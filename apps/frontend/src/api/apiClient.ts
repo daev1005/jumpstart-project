de01ed802d64a086773bc9fcdf06e9611f5bd039
Kenny Jung <dukyoung01@naver.com>
2023-09-12 22:33:16 +0900

    Add sample backend route call

---
+++ apps/frontend/src/api/apiClient.ts
@@ -0,0 +1,38 @@
+import axios, { type AxiosInstance } from 'axios';
+
+const defaultBaseUrl =
+  import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3000';
+
+export class ApiClient {
+  private axiosInstance: AxiosInstance;
+
+  constructor(baseURL: string = defaultBaseUrl) {
+    this.axiosInstance = axios.create({ baseURL });
+  }
+
+  public async getHello(): Promise<string> {
+    return this.get('/api') as Promise<string>;
+  }
+
+  private async get(path: string): Promise<unknown> {
+    return this.axiosInstance.get(path).then((response) => response.data);
+  }
+
+  private async post(path: string, body: unknown): Promise<unknown> {
+    return this.axiosInstance
+      .post(path, body)
+      .then((response) => response.data);
+  }
+
+  private async patch(path: string, body: unknown): Promise<unknown> {
+    return this.axiosInstance
+      .patch(path, body)
+      .then((response) => response.data);
+  }
+
+  private async delete(path: string): Promise<unknown> {
+    return this.axiosInstance.delete(path).then((response) => response.data);
+  }
+}
+
+export default new ApiClient();
--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,14 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { useEffect } from 'react';
 
-import NxWelcome from './nx-welcome';
+import apiClient from '../api/apiClient';
 
-export function App() {
-  return (
-    <div>
-      <NxWelcome title="frontend" />
-    </div>
-  );
-}
+export const App: React.FC = () => {
+  useEffect(() => {
+    apiClient.getHello().then((res) => console.log(res));
+  }, []);
+
+  return <div>Hello World!</div>;
+};
 
 export default App;
