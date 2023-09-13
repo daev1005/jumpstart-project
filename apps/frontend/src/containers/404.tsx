d2f8e38f20dd07ebdd732f098011f5d6f38b5d5b
Kenny Jung <dukyoung01@naver.com>
2023-09-13 09:12:46 +0900

    Fix typescript error on 404 page

--- apps/frontend/src/containers/404.tsx
+++ apps/frontend/src/containers/404.tsx
@@ -1,15 +1,16 @@
 import { useRouteError } from 'react-router-dom';
 
+import { getErrorMessage } from 'utils/router';
+
 const NotFound: React.FC = () => {
-  const error = useRouteError();
-  console.error(error);
+  const errorMessage = getErrorMessage(useRouteError());
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errorMessage}</i>
       </p>
     </div>
   );
